c60f9327136395f79719a3400a1782a401854852
Reimu <remihuguenot89@gmail.com>
2021-10-08 18:31:49 +0200

    Ajout de la creation de la trable reduite

--- test/src/main/java/Ellipsys/test/App.java
+++ test/src/main/java/Ellipsys/test/App.java
@@ -13,8 +13,32 @@ public class App
     public static void main( String[] args ) throws SQLException
     {
         DatabaseConnection database = new DatabaseConnection("jdbc:sqlite:ellipsys_test_db.db3");
-    	
-        
+    	ArrayList<String> collones = new ArrayList<String>();
+    	collones.add("id");
+    	collones.add("trf");
+    	collones.add("tgtTb");
+    	collones.add("tgtLab");
+    	collones.add("srcTb");
+    	collones.add("srcLab");
+    	collones.add("impact");
+
+		String originalTableName= "oa_trf_src";
+		ArrayList<String> tableCreated = new ArrayList<String>();
+		tableCreated.add("oa_trf_src_id_lkp");
+		tableCreated.add("oa_trf_src_trf_lkp");
+		tableCreated.add("oa_trf_src_tgtTb_lkp");
+		tableCreated.add("oa_trf_src_tgtLab_lkp");
+		tableCreated.add("oa_trf_src_srcTb_lkp");
+		tableCreated.add("oa_trf_src_srcLab_lkp");
+		tableCreated.add("oa_trf_src_impact_lkp");
+    
+		
+		
+		
+		
+		database.createTableRed( originalTableName,collones, tableCreated) ;
+
+		
     	
     }
 }
