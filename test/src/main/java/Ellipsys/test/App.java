22d92a964103a75b47a04291f4175c4d644cbf56
Reimu <remihuguenot89@gmail.com>
2021-10-08 09:53:45 +0200

    project initialisation

---
+++ test/src/main/java/Ellipsys/test/App.java
@@ -0,0 +1,13 @@
+package Ellipsys.test;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
